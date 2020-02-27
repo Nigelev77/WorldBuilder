3105dea47efacfda273e8e4d095c95c3ea488ab7
Nigelev77 <Nigelev77@users.noreply.github.com>
2020-02-27 22:05:41 +0000

    error
    See last commit message for full details

--- src/utils/Maths.java
+++ src/utils/Maths.java
@@ -1,6 +1,7 @@
 package utils;
 
 import org.joml.Matrix4f;
+import org.joml.Vector3f;
 
 import player.Camera;
 
@@ -8,8 +9,8 @@ public class Maths {
 	
 	public static Matrix4f setProjectionAndView(Matrix4f viewMatrix, Matrix4f projection, Camera camera) {
 		
-		
-		
+		Matrix4f.projViewFromRectangle(camera.getPosition(), null, null, null, 700, false, projection, viewMatrix);
+		//NEED TO FINISH THIS
 		return null;
 		
 	}
